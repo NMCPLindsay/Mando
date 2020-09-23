d7d48034e3d0fea7988b0b6387eb89a5675d3785
Michelle Vanwettere <57185740+Kaulitza@users.noreply.github.com>
2020-09-23 15:49:55 -0400

    Add files via upload

--- PresentationLayer/Views/MichelleView.xaml.cs
+++ PresentationLayer/Views/MichelleView.xaml.cs
@@ -24,5 +24,9 @@ namespace MandalorianDB.PresentationLayer
             InitializeComponent();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
